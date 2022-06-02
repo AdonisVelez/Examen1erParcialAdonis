32161ef6564a902d9695f68a61f3a543176f98dd
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-06-02 18:07:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Examen1erparcialAdonis
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            EstudiantePrimaria primaria = new EstudiantePrimaria();
+            primaria.Datos("Jaun Carlos","Cevallos Vera","Luis Felipe Chavez #76",1316765428);
+            primaria.ImprimirDatos();
+
+            EstudianteSecundaria secundaria = new EstudianteSecundaria();
+            secundaria.Datos("Pedro Anthonio","Vera Cevallos","Luis Felipe Chavez #76",1316452874);
+            secundaria.DatosExtra(3);
+            secundaria.ImprimirDatos();
+
+            EstudianteUniversitario universitario = new EstudianteUniversitario();
+            universitario.Datos("Carlos Alberto","Cedeño Intriago","ULEAM",1316465827);
+            universitario.DatosEx(5);
+            universitario.ImprimirDatos();
+        }
+    }
+}
